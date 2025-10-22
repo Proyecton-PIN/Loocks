b8e672fdbddcc25320f51f9a28eca80f6e1466b6
ismelen <ismael.ma34k@gmail.com>
2025-10-22 16:14:23 +0200

    feat: mensaje en consola con link a documentación

---
+++ backend/loocks/src/main/java/pin/loocks/api/loggers/StartupLogger.java
@@ -0,0 +1,22 @@
+package pin.loocks.api.loggers;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartupLogger implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) {
+        String port = System.getProperty("server.port", "8080");
+        String contextPath = System.getProperty("server.servlet.context-path", "");
+        if (contextPath.equals("/")) contextPath = "";
+
+        System.out.println("\n----------------------------------------------------------");
+        System.out.println("Swagger UI disponible en:");
+        System.out.println("http://localhost:" + port + contextPath + "/swagger-ui/index.html");
+        System.out.println("OpenAPI JSON:");
+        System.out.println("http://localhost:" + port + contextPath + "/v3/api-docs");
+        System.out.println("----------------------------------------------------------\n");
+    }
+}
