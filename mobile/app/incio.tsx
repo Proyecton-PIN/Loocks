0c65f6d57c55aa723f7025bf09a203f58fca0095
dnavcar <dnavcar@upv.edu.es>
2025-11-19 15:59:33 +0100

    diseño inicio

--- mobile/app/incio.tsx
+++ mobile/app/incio.tsx
@@ -1,33 +1,101 @@
 import { router } from 'expo-router';
 import React from 'react';
-import {
-    Text,
-    TouchableOpacity,
-    View
-} from 'react-native';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
 
 export default function Inicio() {
+  return (
+    <View className="flex-1 bg-[#F3F3F3]">
+      <View className="absolute top-0 left-0 w-full h-[60%] ">
+        <Image
+          source={require('../assets/imagenes-login/camiseta.png')}
+          className="absolute opacity-100"
+          style={{
+            width: 232,
+            height: 234,
+            top: 128.25,
+            left: 168.02,
+            transform: [{ rotate: '-12.12deg' }],
+          }}
+        />
 
-  return ( 
-    <View className="flex-1 justify-center items-center bg-[#F3F3F3] px-4">
-       <View className="w-300px y-300px mb-10">
-        <TouchableOpacity
-          onPress={() => {}}
-          className=" items-center w-full 0 rounded-xl py-2 mb-4 bg-[#5639F8]"
-        >
-          <Text className="flex-1 text-center text-base font-semibold text-gray-800">
-            Empezar...
+        <Image
+          source={require('../assets/imagenes-login/chaqueta.png')}
+          className="absolute opacity-100"
+          style={{
+            width: 249,
+            height: 311,
+            top: -59,
+            left: -122,
+            transform: [{ rotate: '-12.93deg' }],
+          }}
+        />
+        <Image
+          source={require('../assets/imagenes-login/zapatillas.png')}
+          className="absolute opacity-100"
+          style={{
+            width: 300,
+            height: 110,
+            top: 290,
+            left: -113,
+            transform: [{ rotate: '148.67deg' }],
+          }}
+        />
+        <Image
+          source={require('../assets/imagenes-login/bolso.png')}
+          className="absolute opacity-100"
+          style={{
+            width: 145.56,
+            height: 169.85,
+            top: -87,
+            left: 197,
+            transform: [{ rotate: '18.05deg' }],
+          }}
+        />
+        <Image
+          source={require('../assets/imagenes-login/pantalon.png')}
+          className="absolute opacity-100"
+          style={{
+            width: 177,
+            height: 358,
+            top: 361,
+            left: 250,
+            transform: [{ rotate: '-13.51deg' }],
+          }}
+        />
+      </View>
+
+      <View className="flex-1 justify-end items-center px-4 pb-12">
+        <View className="w-full max-w-[350px] bg-white rounded-3xl shadow-xl p-6">
+          <Text className="text-center text-3xl font-bold text-gray-900 mb-1">
+            Loocks
           </Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => {router.replace('/login')}}
-          className=" items-center w-full border border-gray-300 rounded-xl py-2 mb-6 bg-[#F3F3F3]"
-        >
-          <Text className="flex-1 text-center text-base font-semibold text-gray-800">
-            Iniciar Sesión
+          <Text className="text-center text-base text-gray-700 mb-6">
+            Vestir bien, sin esfuerzo
           </Text>
-        </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              /* Registro*/
+            }}
+            className="w-full rounded-xl py-2 mb-3 bg-[#5639F8]"
+          >
+            <Text className="text-center text-base font-semibold text-white">
+              Empezar...
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              router.replace('/login');
+            }}
+            className="w-full border border-gray-300 rounded-xl py-2 bg-[#F3F3F3]"
+          >
+            <Text className="text-center text-base font-semibold text-gray-400">
+              Iniciar sesión...
+            </Text>
+          </TouchableOpacity>
         </View>
+      </View>
     </View>
   );
 }
